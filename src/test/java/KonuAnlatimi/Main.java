73b28f97e2374dbdd11d64c43498071d1dfc0b76
yasemindgn <jasmindgn06@gmail.com>
2020-01-06 22:43:57 -0600

    added project

---
+++ src/test/java/KonuAnlatimi/Main.java
@@ -0,0 +1,18 @@
+package KonuAnlatimi;
+
+public class Main {
+    static int result;
+    public static void main(String[] args) {
+         result = 5+3;
+        System.out.println(result);
+
+    }
+
+    public static void tryingBest() {
+        System.out.println(result);
+
+    }
+}
+class OtomobilParcasi{
+
+}
